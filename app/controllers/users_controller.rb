b590db232a8500ee6da7e0755ced87f87055f2c0
ryohei19 <ryohei.basket@gmail.com>
2021-08-04 14:39:09 +0000

    create relationship

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,8 @@ class UsersController < ApplicationController
     end
   end
 
+
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ Rails.application.routes.draw do
 
     resources :book_comments,only:[:create,:destroy]
   end
-  resources :users
+
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
